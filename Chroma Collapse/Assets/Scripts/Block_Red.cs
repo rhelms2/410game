b2ef75578ffb9215030fa445ffd1b8ea1dee97ba
sshina <sshinaul@uoregon.edu>
2024-04-28 15:36:21 -0700

    foo

--- Chroma Collapse/Assets/Scripts/Block_Red.cs
+++ Chroma Collapse/Assets/Scripts/Block_Red.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 public class Block_Red : GLOBAL_color
 {
     private bool isOn;
-<<<<<<< HEAD
     //Rigidbody blocks;
     //BoxCollider bc;
-=======
-    public Rigidbody rb;
-    //Collider obj_Collider;
->>>>>>> ba9bbf324b12d6b974092becfcda14fa419f5ad3
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +15,6 @@ public class Block_Red : GLOBAL_color
         //bc = GetComponent<BoxCollider>();
         Physics.IgnoreLayerCollision(9, 11, true);
         isOn = false;
-<<<<<<< HEAD
-=======
-        //rb.detectCollisions = false;
-
-        //obj_Collider = GetComponent<Collider>();
->>>>>>> ba9bbf324b12d6b974092becfcda14fa419f5ad3
     }
 
     // Update is called once per frame
@@ -35,7 +24,6 @@ public class Block_Red : GLOBAL_color
         if (col_num == 1 || col_num == 2 || col_num == 6){
             if (isOn == false){
                 isOn = true;
-<<<<<<< HEAD
                 Physics.IgnoreLayerCollision(9, 11, false);
             }
         }else{
@@ -43,20 +31,6 @@ public class Block_Red : GLOBAL_color
                 isOn = false;
                 Physics.IgnoreLayerCollision(9, 11, true);
             }
-=======
-
-                rb.detectCollisions = true;
-                //obj_Collider.enabled = true;
-
-                 
-
-            }
-        }else{
-             isOn = false;
-             rb.detectCollisions = false;
-
-            //obj_Collider.enabled = false;
->>>>>>> ba9bbf324b12d6b974092becfcda14fa419f5ad3
         }
     }
 }
