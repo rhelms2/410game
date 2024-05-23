38fb083cc4d6846c75ca549518d2e585e1607fc8
ereinha3 <ethanreinhart@gmail.com>
2024-05-22 17:49:44 -0700

    tryna make recoil

--- Chroma Collapse/Assets/Scripts/RayGun.cs
+++ Chroma Collapse/Assets/Scripts/RayGun.cs
@@ -7,20 +7,26 @@ public class RayGun : MonoBehaviour
     private float m_shootRateTimeStamp;
 
     public GameObject m_shotPrefab;
+    public GameObject gun;
 
     RaycastHit hit;
     float range = 1000.0f;
 
+    float maxRecoil_x = -20f;
+    private float recoilSpeed = 2f;
+    // in seconds
+    public float recoil = 0.2f;
 
     void Update()
     {
-
+        recoiling();
         if (Input.GetMouseButton(0))
         {
             if (Time.time > m_shootRateTimeStamp)
             {
                 shootRay();
                 m_shootRateTimeStamp = Time.time + shootRate;
+                recoil += 0.2f;
             }
         }
 
@@ -42,6 +48,30 @@ public class RayGun : MonoBehaviour
 
     }
 
+    //public void StartRecoil(float recoilParam, float maxRecoil_xParam, float recoilSpeedParam)
+    //{
+    //    // in seconds
+    //    recoil = recoilParam;
+    //    maxRecoil_x = maxRecoil_xParam;
+    //    recoilSpeed = recoilSpeedParam;
+    //}
+
+    void recoiling()
+    {
+        if (recoil > 0f)
+        {
+            Quaternion maxRecoil = Quaternion.Euler(maxRecoil_x, 0f, 0f);
+            // Dampen towards the target rotation
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, maxRecoil, Time.deltaTime * recoilSpeed);
+            recoil -= Time.deltaTime;
+        }
+        else
+        {
+            recoil = 0f;
+            // Dampen towards the target rotation
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.identity, Time.deltaTime * recoilSpeed / 2);
+        }
+    }
 
 
 }
