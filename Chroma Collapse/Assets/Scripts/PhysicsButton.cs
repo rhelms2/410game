0d5ed8d5032d591f041ee727b61f09ae8d19d46b
ereinha3 <ethanreinhart@gmail.com>
2024-05-28 10:47:45 -0700

    plinko done!

--- Chroma Collapse/Assets/Scripts/PhysicsButton.cs
+++ Chroma Collapse/Assets/Scripts/PhysicsButton.cs
@@ -12,103 +12,132 @@ public class PhysicsButton : MonoBehaviour
     public GameObject pedestal;
     public GameObject keyCard;
     private GameObject ball;
-    private Vector3 delta_position = new Vector3(0f, 0.05f, 0f);
-    private Vector3 currentPlatformPosition;
+    private Vector3 deltaButtonPosition = new Vector3(0f, 0.05f, 0f);
+    private Vector3 deltaPedestalPosition = new Vector3(0f, 2.5f, 0f);
+    private Vector3 targetPedestalPosition;
+    private Vector3 targetKeyCardPosition;
     private Vector3 targetPlatformPosition;
     public float platformSpeed = 2f;
-    private Vector3 lastPosition;
-    private Vector3 ballSpeedVector;
-    private float ballSpeed;
     private float time_passed = 0;
     private bool pressed = false;
     private bool active = false;
-    private bool platformMoving = true;
+    private bool platformMoving = false;
+    private bool pedestalMoving = false;
+    private bool keyCardMoving = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        lastPosition = ballSpawner.transform.position;
-        targetPlatformPosition = new Vector3(currentPlatformPosition.x, currentPlatformPosition.y, currentPlatformPosition.z - 2.7f);
+        targetPlatformPosition = new Vector3(platform.transform.position.x, platform.transform.position.y, platform.transform.position.z - 2.7f);
+        targetPedestalPosition = pedestal.transform.position + deltaPedestalPosition;
+        targetKeyCardPosition = keyCard.transform.position + deltaPedestalPosition;
     }
 
     void Update()
     {
-        currentPlatformPosition = platform.transform.position;
-
         if (pressed == true)
         {
-            Debug.Log(time_passed);
             time_passed += Time.deltaTime;
             if (time_passed >= 0.5f)
             {
                 pressed = false;
                 time_passed = 0;
-                button.transform.localPosition += delta_position;
+                button.transform.localPosition += deltaButtonPosition;
             }
 
         }
+
+        if (active)
+        {
+            if (ball.transform.localPosition.y < -18.5f)
+            {
+                float z = ball.transform.localPosition.z;
+                Debug.Log("z" + z);
+                if (z < 12 && z > 8)
+                {
+                    Debug.Log("win!");
+                    platformMoving = true;
+                }
+                active = false;
+            }
+        }
+
         if (platformMoving)
         {
-            platform.transform.position = Vector3.MoveTowards(currentPlatformPosition, targetPlatformPosition, platformSpeed * Time.deltaTime);
-        }
-        if (currentPlatformPosition == targetPlatformPosition){
-            platformMoving = false;
-        }
-
-
-
-    }
-
-    IEnumerator ballCheck()
-    {
-        while (active)
-        {
-            Debug.Log("Coroutine started");
-            lastPosition = ball.transform.position;
-            yield return new WaitForEndOfFrame();
-            Debug.Log("After WaitForEndOfFrame");
-
-            Debug.Log("Current Position: " + ball.transform.position);
-            Debug.Log("Last Position: " + lastPosition);
-
-            ballSpeedVector = (ball.transform.position - lastPosition) / Time.deltaTime;
-            ballSpeed = ballSpeedVector.magnitude;
-            Debug.Log("Ball Speed: " + ballSpeed);
-
-            if (Mathf.Approximately(ballSpeed, 0f) && ball.transform.localPosition.y < -18.5f)
+            platform.transform.position = Vector3.MoveTowards(platform.transform.position, targetPlatformPosition, platformSpeed * Time.deltaTime);
+            if (platform.transform.position == targetPlatformPosition)
             {
-                Debug.Log("Destroying Ball");
-                Destroy(ball, 0f);
-                active = false;
-                lastPosition = ballSpawner.transform.position;
+                platformMoving = false;
+                pedestalMoving = true;
+                keyCardMoving = true;
             }
-
-            yield return null; // Ensure the coroutine runs continuously
         }
+
+        if (pedestalMoving)
+        {
+            pedestal.transform.position = Vector3.MoveTowards(pedestal.transform.position, targetPedestalPosition, platformSpeed * Time.deltaTime);
+            if (pedestal.transform.position == targetPedestalPosition)
+            {
+                pedestalMoving = false;
+            }
+        }
+
+        if (keyCardMoving)
+        {
+            keyCard.transform.position = Vector3.MoveTowards(keyCard.transform.position, targetKeyCardPosition, platformSpeed * Time.deltaTime);
+            if (keyCard.transform.position == targetKeyCardPosition)
+            {
+                keyCardMoving = false;
+            }
+        }
+
+
+
+
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.tag);
         if (other.gameObject.tag == "Player" || other.gameObject.tag == "Bullet")
         {
-            Debug.Log("Collided");
-            if (pressed == false){
+            if (pressed == false)
+            {
                 pressed = true;
-                button.transform.localPosition -= delta_position;
-                Debug.Log("changed position of button");
+                button.transform.localPosition -= deltaButtonPosition;
                 if (!active)
                 {
                     ball = Instantiate(ballPrefab, ballSpawner.transform.position, ballSpawner.transform.rotation);
                     ball.transform.parent = ballSpawner.transform;
-                    lastPosition = ball.transform.position;
                     active = true;
-                    StartCoroutine(ballCheck());
                 }
-                
+
             }
-            
+
         }
     }
+
+    //IEnumerator ballCheck()
+    //{
+    //    while (active)
+    //    {
+    //        //Debug.Log("Coroutine started");
+    //        //lastPosition = ball.transform.position;
+    //        //yield return new WaitForEndOfFrame();
+    //        //Debug.Log("After WaitForEndOfFrame");
+
+    //        //Debug.Log("Current Position: " + ball.transform.position);
+    //        //Debug.Log("Last Position: " + lastPosition);
+
+    //        //ballSpeedVector = (ball.transform.position - lastPosition);
+    //        //ballSpeed = ballSpeedVector.magnitude / Time.deltaTime;
+    //        //Debug.Log("Ball Speed: " + ballSpeed);
+
+
+
+    //        yield return null; // Ensure the coroutine runs continuously
+    //    }
+    //}
+
+
 
 }
