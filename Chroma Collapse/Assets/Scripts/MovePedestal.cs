1d99b826e07f32215b6f09e9027cf6dd77bf0927
ereinha3 <ethanreinhart@gmail.com>
2024-05-30 17:42:51 -0700

    random shit

---
+++ Chroma Collapse/Assets/Scripts/MovePedestal.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePedestal : MonoBehaviour
+{
+    public GameObject platform;
+    public GameObject pedestal;
+    public GameObject item;
+    private Vector3 deltaButtonPosition = new Vector3(0f, 0.05f, 0f);
+    private Vector3 deltaPedestalPosition = new Vector3(0f, 2.5f, 0f);
+    private Vector3 targetPedestalPosition;
+    private Vector3 targetItemPosition;
+    private Vector3 targetPlatformPosition;
+    public float platformSpeed = 2f;
+    public bool platformMoving = false;
+    private bool pedestalMoving = false;
+    private bool keyCardMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPlatformPosition = new Vector3(platform.transform.position.x, platform.transform.position.y, platform.transform.position.z - 2.7f);
+        targetPedestalPosition = pedestal.transform.position + deltaPedestalPosition;
+        targetItemPosition = item.transform.position + deltaPedestalPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (platformMoving)
+        {
+            platform.transform.position = Vector3.MoveTowards(platform.transform.position, targetPlatformPosition, platformSpeed * Time.deltaTime);
+            if (platform.transform.position == targetPlatformPosition)
+            {
+                platformMoving = false;
+                pedestalMoving = true;
+                keyCardMoving = true;
+            }
+        }
+
+        if (pedestalMoving)
+        {
+            pedestal.transform.position = Vector3.MoveTowards(pedestal.transform.position, targetPedestalPosition, platformSpeed * Time.deltaTime);
+            if (pedestal.transform.position == targetPedestalPosition)
+            {
+                pedestalMoving = false;
+            }
+        }
+
+        if (keyCardMoving)
+        {
+            item.transform.position = Vector3.MoveTowards(item.transform.position, targetItemPosition, platformSpeed * Time.deltaTime);
+            if (item.transform.position == targetItemPosition)
+            {
+                keyCardMoving = false;
+            }
+        }
+    }
+}
--- Chroma Collapse/Assets/Scripts/PhysicsButton.cs
+++ Chroma Collapse/Assets/Scripts/PhysicsButton.cs
@@ -2,35 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
+using UnityEngine.UI;
 
 public class PhysicsButton : MonoBehaviour
 {
     public GameObject button;
     public GameObject ballPrefab;
     public GameObject ballSpawner;
-    public GameObject platform;
-    public GameObject pedestal;
-    public GameObject keyCard;
     private GameObject ball;
-    private Vector3 deltaButtonPosition = new Vector3(0f, 0.05f, 0f);
-    private Vector3 deltaPedestalPosition = new Vector3(0f, 2.5f, 0f);
-    private Vector3 targetPedestalPosition;
-    private Vector3 targetKeyCardPosition;
-    private Vector3 targetPlatformPosition;
-    public float platformSpeed = 2f;
     private float time_passed = 0;
     private bool pressed = false;
     private bool active = false;
-    private bool platformMoving = false;
-    private bool pedestalMoving = false;
-    private bool keyCardMoving = false;
+    public MovePedestal movePedestal;
+    private Vector3 deltaButtonPosition = new Vector3(0f, 0.05f, 0f);
+
+    public string optional_overlay_text;
+    private TextMeshProUGUI text_target;
+    private Image target_display;
+    private bool in_range = false;
+    private bool fPressed = false;
+
+    //public GameObject platform;
+    //public GameObject pedestal;
+    //public GameObject item;
+    //private Vector3 deltaPedestalPosition = new Vector3(0f, 2.5f, 0f);
+    //private Vector3 targetPedestalPosition;
+    //private Vector3 targetItemPosition;
+    //private Vector3 targetPlatformPosition;
+    //public float platformSpeed = 2f;
+    //private bool platformMoving = false;
+    //private bool pedestalMoving = false;
+    //private bool keyCardMoving = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        targetPlatformPosition = new Vector3(platform.transform.position.x, platform.transform.position.y, platform.transform.position.z - 2.7f);
-        targetPedestalPosition = pedestal.transform.position + deltaPedestalPosition;
-        targetKeyCardPosition = keyCard.transform.position + deltaPedestalPosition;
+        //targetPlatformPosition = new Vector3(platform.transform.position.x, platform.transform.position.y, platform.transform.position.z - 2.7f);
+        //targetPedestalPosition = pedestal.transform.position + deltaPedestalPosition;
+        //targetItemPosition = item.transform.position + deltaPedestalPosition;
+
+        target_display = GameObject.FindWithTag("Display Port").transform.GetChild(1).gameObject.GetComponent<Image>();
+        text_target = GameObject.FindWithTag("Display Port").transform.GetChild(2).gameObject.GetComponent<TextMeshProUGUI>();
     }
 
     void Update()
@@ -53,47 +66,38 @@ public class PhysicsButton : MonoBehaviour
             {
                 float z = ball.transform.localPosition.z;
                 Debug.Log("z" + z);
-                if (z < 12.5 && z > 7.5)
+                if (z < 13 && z > 7)
                 {
                     Debug.Log("win!");
-                    platformMoving = true;
+                    movePedestal.platformMoving = true;
                 }
                 active = false;
             }
         }
 
-        if (platformMoving)
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the other collider belongs to the player object
+        if (other.CompareTag("Player"))
         {
-            platform.transform.position = Vector3.MoveTowards(platform.transform.position, targetPlatformPosition, platformSpeed * Time.deltaTime);
-            if (platform.transform.position == targetPlatformPosition)
+            in_range = true;
+            if (Input.GetKeyDown("f"))
             {
-                platformMoving = false;
-                pedestalMoving = true;
-                keyCardMoving = true;
+                active = true;
             }
         }
+    }
 
-        if (pedestalMoving)
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the other collider belongs to the player object
+        if (other.CompareTag("Player"))
         {
-            pedestal.transform.position = Vector3.MoveTowards(pedestal.transform.position, targetPedestalPosition, platformSpeed * Time.deltaTime);
-            if (pedestal.transform.position == targetPedestalPosition)
-            {
-                pedestalMoving = false;
-            }
+            in_range = false;
         }
-
-        if (keyCardMoving)
-        {
-            keyCard.transform.position = Vector3.MoveTowards(keyCard.transform.position, targetKeyCardPosition, platformSpeed * Time.deltaTime);
-            if (keyCard.transform.position == targetKeyCardPosition)
-            {
-                keyCardMoving = false;
-            }
-        }
-
-
-
-
     }
 
     private void OnCollisionEnter(Collision other)
@@ -103,16 +107,27 @@ public class PhysicsButton : MonoBehaviour
             if (pressed == false)
             {
                 pressed = true;
-                button.transform.localPosition -= deltaButtonPosition;
-                if (!active)
-                {
-                    ball = Instantiate(ballPrefab, ballSpawner.transform.position, ballSpawner.transform.rotation);
-                    ball.transform.parent = ballSpawner.transform;
-                    active = true;
-                }
+                //button.transform.localPosition -= deltaButtonPosition;
+                //if (!active)
+                //{
+                //    ball = Instantiate(ballPrefab, ballSpawner.transform.position, ballSpawner.transform.rotation);
+                //    ball.transform.parent = ballSpawner.transform;
+                //    active = true;
+                //}
 
             }
 
+        }
+    }
+
+    private void makeBall()
+    {
+        button.transform.localPosition -= deltaButtonPosition;
+        if (!active)
+        {
+            ball = Instantiate(ballPrefab, ballSpawner.transform.position, ballSpawner.transform.rotation);
+            ball.transform.parent = ballSpawner.transform;
+            active = true;
         }
     }
 
