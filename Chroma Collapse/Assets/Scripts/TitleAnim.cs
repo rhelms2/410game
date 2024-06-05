10ee1f5e480fe0c205f1601755a61dee39069066
sshina <sshinaul@uoregon.edu>
2024-06-04 20:29:32 -0700

    Finished title screen

--- Chroma Collapse/Assets/Scripts/TitleAnim.cs
+++ Chroma Collapse/Assets/Scripts/TitleAnim.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class TitleAnim : MonoBehaviour
 {
     public Animator anim;
+    public GameObject visuals;
+    int tick = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        visuals.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tick++;
+        if (tick > 410) {
+            if (transform.position.y >= -479) transform.position -= new Vector3(0, 0.5f, 0);
+            else {
+                visuals.SetActive(true);
+                anim.speed = 0f;
+                anim.Play("title", 0, 0f);
+            }
+        }
     }
 }
